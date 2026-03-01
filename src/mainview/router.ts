6833ae9bc218fef90b8a9da714150321eb7b31c9
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-01 05:15:47 -0500

    Disabled eslint errors for non project code

--- src/mainview/router.ts
+++ src/mainview/router.ts
@@ -1,6 +1,7 @@
 import { createRouter } from 'sv-router';
 import App from './pages/App.svelte';
 
+// eslint-disable-next-line @typescript-eslint/unbound-method
 export const { p, navigate, isActive, route } = createRouter({
 	'/': App
 });
